434a9b9e807a94e1501b536741c4b8c99cc0f493
Gabriel Neimester <gneimester@hotmail.com>
2024-12-31 19:23:19 -0300

    adicionando autenticacao

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,5 +1,15 @@
+import { signOut } from '@/auth';
+import { Button } from '@nextui-org/button';
+
 export default function Shop() {
     return (
-        <h1>This is the shop page</h1>
+        <>
+            <h1>This is the shop page</h1>
+            <Button onPress={async () => {
+                'use server';
+                await signOut();
+            }}>Log out</Button>
+        </>
+
     )
 }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,12 +1,62 @@
-import { Button } from '@nextui-org/button'
-import { Input } from '@nextui-org/input'
+'use client';
+
+import { Button } from '@nextui-org/button';
+import { Input } from '@nextui-org/input';
+import { useState } from 'react';
+import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
-    return ( 
-        <form action="">
-            <Input label="Email" placeholder="Enter your email" type="email" radius='full'></Input>
-            <Input label="Password" placeholder="Enter your password" type='password' radius='full'></Input>
-            <Button color="primary" variant="solid">Botão NextUI</Button>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        setIsPending(true);
+        setErrorMessage('');
+
+        const formData = new FormData();
+        formData.append('email', email);
+        formData.append('password', password);
+
+        try {
+            const error = await authenticate(formData);
+            if (error) {
+                setErrorMessage(error);
+            }
+        } catch (error) {
+            setErrorMessage(`${error} erro`);
+        } finally {
+            setIsPending(false);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                label="Email"
+                placeholder="Enter your email"
+                type="email"
+                radius="full"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+                radius="full"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button color="primary" variant="solid" aria-disabled={isPending} type="submit">
+                Login
+            </Button>
+            {errorMessage && (
+                <p className="text-sm text-red-500">{errorMessage}</p>
+            )}
         </form>
-    )
+    );
 }
